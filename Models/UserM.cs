5bcf684c1dbc59f6cc05c872293e2ea0bbad56a2
Vladimir Živković <vlaja432@gmail.com>
2024-12-01 16:18:11 +0100

    drugi komit - dodat folder models i klasa UserM

---
+++ Models/UserM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentilatorNovi1dec24.Models
+{
+    public class UserM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
